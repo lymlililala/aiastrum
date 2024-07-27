8343bb2e30e5d1ef63e638af0c38b5e38342eb24
banan <985097752@qq.com>
2024-07-27 17:09:52 -0400

    adjusted size of table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,14 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="relative w-full max-w-4xl h-96 rounded-full overflow-hidden shadow-lg">
+      <div className="relative w-full max-w-7xl h-[700px] rounded-full overflow-hidden shadow-lg">
         <img
           src="/table.jpg"
           alt="Tarot Table"
           className="w-full h-full object-cover"
         />
-        <div className="absolute inset-0 flex justify-center items-center">
-          <div className="w-3/4 h-3/4 bg-white/50 rounded-full flex justify-center items-center">
-            {/* Add components here */}
-          </div>
+        <div className="w-4/5 h-4/5 bg-white/50 rounded-full flex justify-center items-center">
+          {/* Add components here */}
         </div>
       </div>
     </div>
