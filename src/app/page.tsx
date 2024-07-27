6aa3aae192aa401d7dc3af88096ab8e77f09c83c
banan <985097752@qq.com>
2024-07-27 19:20:54 -0400

    adjusted the view again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"; // This marks the file as a Client Component
 
-import { useState } from 'react';
+import React, { useState } from "react";
 
 const HomePage = () => {
   const [shuffledCards, setShuffledCards] = useState<string[]>([]);
@@ -22,43 +22,41 @@ const HomePage = () => {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      {/* Display shuffled cards */}
+      <div className="absolute z-10 flex space-x-4">
+        {shuffledCards.map((card, index) => (
+          <img
+            key={index}
+            src={card}
+            alt={`Card ${index + 1}`}
+            className="h-48 w-32"
+          />
+        ))}
+      </div>
+      {/* Button to shuffle cards */}
+      <button
+        onClick={shuffleCards}
+        className="absolute left-1/2 top-0 z-20 mt-4 -translate-x-1/2 transform rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+      >
+        Tell me my fortune
+      </button>
+      {/* <img src="/table2.jpg" alt="Tarot Table" className="w-full h-full object-cover z-10" /> */}
       <div className="relative h-[700px] w-full max-w-7xl overflow-hidden rounded-full shadow-lg">
+        <img
+          src="/table2.jpg"
+          alt="Tarot Table"
+          className="z-10 h-full w-full object-cover"
+        />
 
-        {/* Table Image */}
-        <img src="/table2.jpg" alt="Tarot Table" className="absolute inset-0 w-full h-full object-cover z-0" />
-
-        {/* Circle Overlay */}
-        <div className="absolute inset-0 flex items-center justify-center z-10">
+        <div className="absolute inset-0 z-0 flex items-center justify-center">
           <CircleAstrologyIcons />
         </div>
 
-        {/* Button to shuffle cards */}
-        <button
-          onClick={shuffleCards}
-          className="absolute z-20 rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
-        >
-          Tell me my fortune
-        </button>
-
-        {/* Display shuffled cards */}
-        <div className="absolute inset-0 flex items-center justify-center z-30">
-          <div className="flex space-x-4">
-            {shuffledCards.map((card, index) => (
-              <img
-                key={index}
-                src={card}
-                alt={`Card ${index + 1}`}
-                className="h-48 w-32"
-              />
-            ))}
-          </div>
-        </div>
-
+        <div className="absolute z-20 flex h-4/5 w-4/5 flex-col items-center justify-center space-y-4 rounded-full bg-white/50"></div>
       </div>
     </div>
   );
 };
-
 
 const CircleAstrologyIcons = () => {
   const radius = 300; // Increased radius for a larger circle
